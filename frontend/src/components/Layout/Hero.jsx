7e7477f772cf32dbf0610827020667b7ae17db7d
sandeep <sandeep.pr3@gmail.com>
2025-09-13 11:32:40 +0530

    final

--- frontend/src/components/Layout/Hero.jsx
+++ frontend/src/components/Layout/Hero.jsx
@@ -11,13 +11,18 @@ const Hero = () => {
       />
       <div className="absolute inset-0 bg-black/5 flex items-center justify-center">
         <div className="text-center text-white p-6">
-          <h1 className="text-4xl md:text-9xl font-bold tracking-tighter uppercase mb-4 ">
+          <h1 className="text-4xl md:text-9xl font-bold tracking-tighter uppercase mb-4">
             Vacation <br /> Ready
-            <p className="text-sm tracking-tighter md:text-lg mb-6">
-              Explore our vacation-ready outfits with fast worldwide shipping
-            </p>
-            <Link to="#" className="bg-white text-gray-950 px-6 py-2 rounded-sm text-lg">Shop Now</Link>
           </h1>
+          <p className="text-sm tracking-tighter md:text-lg mb-6">
+            Explore our vacation-ready outfits with fast worldwide shipping
+          </p>
+          <Link
+            to="#"
+            className="bg-white text-gray-950 px-6 py-2 rounded-sm text-lg"
+          >
+            Shop Now
+          </Link>
         </div>
       </div>
     </section>
